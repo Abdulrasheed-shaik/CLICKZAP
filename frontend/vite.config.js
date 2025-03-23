1d367782c1de733a24f819f24584a9b61e5e5cb3
abdulrasheedshaik122@gmail.com <abdulrasheedshaik122@gmail.com>
2025-03-23 20:00:39 +0530

    url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Backend URL
+        target: 'https://clickzap-1.onrender.com', // Backend URL
         changeOrigin: true,
         secure: false,
       },
